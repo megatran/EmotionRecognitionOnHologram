988e769255b1b2fb811df2ca2f0b91e5aa5253f1
Nhan Tran <nhantrantnt@gmail.com>
2016-09-17 21:18:18 -0600

    added unity-blender facial/emotional recognition

---
+++ EmotionalExpressionUnity/Assets/ShapeManagement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShapeManagement : MonoBehaviour {
+
+    SkinnedMeshRenderer thisSkinnedMeshRenderer;
+    float weightValue = 0f;
+    public float speed = 0f;
+    void Awake()
+    {
+        thisSkinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+    }
+
+    void Update()
+    {
+        if (thisSkinnedMeshRenderer.GetBlendShapeWeight(0) < 100f)
+        {
+            thisSkinnedMeshRenderer.SetBlendShapeWeight(0, weightValue);
+            weightValue += speed;
+        }
+    }
+}
