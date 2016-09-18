a9c06a657a9fbc424e31a303d80f1fddce257c57
Jacob Emmel <jdemmel94@comcast.net>
2016-09-17 23:42:02 -0400

    Initial commit of classes for HTTP service
    `EmotionProcessor` can be added to a Unity project to start a server,
    which will respond to requests with an "it worked" text response.

---
+++ EmotionalExpressionUnity/Assets/EmotionProcessor.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using Assets.WebServer;
+using UnityEngine;
+
+namespace Assets { 
+    public class EmotionProcessor : MonoBehaviour {
+        private const string ReceivingUrl = "http://localhost:8080/emotion/";
+        private HttpServer _httpServer;
+        
+        public void Start () {
+            // Ensure that we can receive http requests through HttpListener
+            if (!HttpListener.IsSupported)
+            {
+                Debug.Log("HttpListener is not supported--the application cannot run properly.\n" +
+                          "Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+
+            _httpServer = new HttpServer(EmotionProcessor.ProcessEmotionRequest, ReceivingUrl);
+            _httpServer.Run();
+            Debug.Log("Now accepting HTTP requests at " + ReceivingUrl);
+        }
+
+        // This medthod is executed by httpServer when it receives a request.
+        // Params:
+        //   HttpListenerRequest request: the HTTP request received by the server
+        // Returns:
+        //   SimpleResponse: the status code for the response, and a string to return
+        public static SimpleResponse ProcessEmotionRequest(HttpListenerRequest request) {
+            int status = 200;
+            string response = "It's working!";
+
+            // TODO: deserialize and process emotion data
+
+            return new SimpleResponse(status, response);
+        }
+
+        public void OnDestroy() {
+            if (_httpServer != null) {
+                _httpServer.Stop();
+            }
+        }
+    }
+}
---
+++ EmotionalExpressionUnity/Assets/WebServer/SimpleResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Assets.WebServer {
+    // A simple response class allowing us to easily set the status code and 
+    // body of the responses from our service.
+    public class SimpleResponse {
+        public int Status {get; set;}
+        public string Response { get; set;}
+
+        public SimpleResponse(int status, string response) {
+            this.Status = status;
+            this.Response = response;
+        }
+    }
+}
